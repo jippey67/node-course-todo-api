f13472aea62674d19ec762bd57c432747f7a942b
Jeroen Walta <jeroenwalta@gmail.com>
2018-10-17 13:15:46 +0200

    added heroku details

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();	
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 
@@ -21,8 +24,31 @@ app.post('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos})
+	}, (e) => {
+		res.status(400).send(e);
+	})
+});
+
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id
+	if (!ObjectID.isValid(id)) {
+	  return res.status(404).send('invalid ID');
+	}
+	Todo.findById(id).then((todo) => {
+	  if (!todo) {
+	    return res.status(404).send('Id not found');
+	  }
+	  res.status(200).send({todo});
+	}).catch((e) => console.log(e));
+
+});
+
 app.listen(3000, () => {
-	console.log('started listening on 3000');
+	console.log(`started listening on ${port}`);
 });
 
 module.exports = {
